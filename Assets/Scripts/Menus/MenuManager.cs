5f99ee3269e67dd2ae5e2abe104e5b5d42e3baa2
txie24 <167393449+txie24@users.noreply.github.com>
2025-06-03 19:29:00 -0700

    Pause Menu Complete

---
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    [Header("Assign these in the Inspector")]
+    public GameObject mainMenu;    
+    public GameObject settingsMenu; 
+    public GameObject achievementsMenu; 
+    public GameObject creditsMenu;
+
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+        achievementsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+    }
+
+
+    public void ShowSettings()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void ShowAchievements()
+    {
+        mainMenu.SetActive(false);
+        achievementsMenu.SetActive(true);
+    }
+
+    public void ShowCredits()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+        achievementsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Note: GetKeyDown (capital “K”)
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        // Use the instance variable, not the class name
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Start");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
